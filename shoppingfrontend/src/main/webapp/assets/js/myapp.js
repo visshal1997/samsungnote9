7bcb5bd9ef6551320e94f8104a7a8bc18ad6a8d2
Sanjana <a.s.sanjana28@gmail.com>
2018-12-04 22:05:56 +0800

    completed the interface theme and other issues

---
+++ shoppingfrontend/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function() {
+
+	// solving the active menu problem
+	switch (menu) {
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+
+	case 'Home':
+		$('#home').addClass('active');
+		break;
+
+	}
+
+});
